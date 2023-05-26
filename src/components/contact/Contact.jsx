1612d96a796b4de3342e618efc1308c338562a1d
Muhoti <strongmuhoti@gmail.com>
2023-05-26 10:07:51 +0300

    comleted contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,10 +2,28 @@ import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
-import {BsWhatsapp} from 'react-icons/bs'
+import { BsWhatsapp } from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_fvj0wev', 'template_xh405s4', form.current, 'HqSf2vnZh9HJ1nHN9')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    
+    e.target.reset()
+  };
+
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -14,27 +32,36 @@ const Contact = () => {
       <div className="container contact_container">
         <div className="contact_options">
           <article className="contact_option">
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact_option-icon'/>
             <h4>Email</h4>
             <h5>strongmuhoti@gmail.com</h5>
-            <a href="mailto:strongmuhoti@gmail.com">Send a message</a>
+            <a href="mailto:strongmuhoti@gmail.com" target='_blank'>Send a message</a>
           </article>
 
           <article className="contact_option">
-            <RiMessengerLine/>
+            <RiMessengerLine className='contact_option-icon'/>
             <h4>Messanger</h4>
-            <h5>Strong Developers</h5>
-            <a href="https://m.me/strong.muhoti.1/">Send a message</a>
+            <h5>Strong Muhoti</h5>
+            <a href="https://m.me/strong.muhoti.1/" target='_blank'>Send a message</a>
           </article>
 
           <article className="contact_option">
-            <BsWhatsapp/>
+            <BsWhatsapp className='contact_option-icon'/>
             <h4>Whatsapp</h4>
-            <h5>strongmuhoti@gmail.com</h5>
-            <a href="https://api.whatsapp.com">Send a message</a>
+            <h5>+254707809592</h5>
+            <a href="https://wa.me/+254707809592" target='_blank'>Send a message</a>
           </article>
+
         </div>
         {/* END OF CONTACT OPTIONS */}
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+
       </div>
     </section>
   )
